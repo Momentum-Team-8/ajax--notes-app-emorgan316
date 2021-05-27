90cf42fd37ff97b2bcc555b754c1f526a95e146f
emorgan316 <elliemorgan96@gmail.com>
2021-05-27 13:46:04 -0400

    just adding

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = 'http://localhost:3000/notes'
 const form = 'http://localhost:3000/notes/'
-const Notepad = document.querySelector('#notepad')
+const Notebook = document.querySelector('#notebook')
 
 {
 fetch (url)
