2f2307ce20f8af848b02ba3049f49f52bb747510
emorgan316 <elliemorgan96@gmail.com>
2021-05-30 18:12:02 -0400

    trying s**t out

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const url = 'http://localhost:3000/notes'
 const form = 'http://localhost:3000/notes/'
 const Notebook = document.querySelector('#notebook')
 
-{
+function listNotes() {
+
 fetch (url)
-.then ((Response) = response.json())
+.then ((response) => response.json())
 .then ((data) => console.log (data))
 }
