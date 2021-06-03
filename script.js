7fec6e70f513b96174e224517e41c360814b039f
emorgan316 <elliemorgan96@gmail.com>
2021-06-03 19:54:13 -0400

    update notes app

--- script.js
+++ script.js
@@ -5,21 +5,39 @@ const Notebook = document.querySelector('#notebook')
 function listNotes() {
     fetch (url)
     .then ((response) => response.json())
-    .then ((data) => console.log (data))
+    .then ((data) => {
+        data.forEach(note => createNoteElement(note));
+    })
 }
+
+function createNoteElement(note){
+    // creating new div for the note
+    let noteDiv =document.createElement('div');
+    noteDiv.setAttribute('id',note.id);
+    // console.log(note.id);
+    noteDiv.innerHTML = `${note.title} <br />${note.body}<br /><button onclick='deleteNote(${note.id})'>delete</button><br />`;
+    document.getElementById('notes').appendChild(noteDiv);
+}
+
 function deleteNote(id) {
     fetch (url + '/' +id, {method: 'delete'})
     .then ((response) => response.json())
-    .then ((data) => console.log (data))
+    .then ((data) => {
+        let note = document.getElementById(id);
+        note.remove();
+    })
 }
 
-// {
-//     "id": 1,
-//     "title": "Confusion is my life",
-//     "body": "This is the body of my note"
-//   }
-function addNote(json) {
+function addNote() {
+    var title = document.getElementById('addinputtitle').value;
+    var body = document.getElementById('addinputbody').value;
+    var json = {'title': title ,'body': body };
     fetch (url, {method: 'post', body: JSON.stringify(json), headers:{'Content-type':'application/json; charset=UTF-8'}})
     .then ((response) => response.json())
-    .then ((data) => console.log (data))
+    .then ((data) => {
+        json.id = data.id;
+        createNoteElement(json)    
+    });
 }
+// this is executing the function to list the notes
+listNotes();
